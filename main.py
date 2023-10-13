b162b32e6dc9f3bbec3be49703cefefa68802cd5
jymstr <jimmy@nostri.ai>
2023-10-13 16:19:24 +0200

    added functionality from checkers-MCTS

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
+import sys
+import numpy as np
+
 from fastapi import Request, FastAPI
 from fastapi.staticfiles import StaticFiles
 from agents import RandomChoiceAgent
-import numpy as np
+
+from utils import states_to_piece_positions
+
+from checkers.Checkers import Checkers
+from checkers.MCTS import MCTS
+from checkers.MCTS import MCTS_Node
 
 
 app = FastAPI()
 app.mount("/game", StaticFiles(directory="static", html= True), name="static")
-
+# zasto dva puta definisemo fastapi?
 api = FastAPI()
 
 ai_agent = RandomChoiceAgent()
+
+# %% Initialize game environment and MCTS class
+# Set MCTS parameters
+mcts_kwargs = {     # Parameters for MCTS used in tournament
+'NN_FN' : 'data/model/Checkers_Model10_12-Feb-2021(14:50:36).h5',
+'UCT_C' : 4,                # Constant C used to calculate UCT value
+'CONSTRAINT' : 'rollout',   # Constraint can be 'rollout' or 'time'
+'BUDGET' : 400,             # Maximum number of rollouts or time in seconds
+'MULTIPROC' : False,        # Enable multiprocessing
+'NEURAL_NET' : False,        # If False uses random rollouts instead of NN
+'VERBOSE' : False,          # MCTS prints search start/stop messages if True
+'TRAINING' : False,         # True if self-play, False if competitive play
+'DIRICHLET_ALPHA' : 1.0,    # Used to add noise to prior probs of actions
+'DIRICHLET_EPSILON' : 0.25, # Fraction of noise added to prior probs of actions  
+'TEMPERATURE_TAU' : 0,      # Initial value of temperature Tau
+'TEMPERATURE_DECAY' : 0,    # Linear decay of Tau per move
+'TEMP_DECAY_DELAY' : 0      # Move count before beginning decay of Tau value
+}
+
+game_env = Checkers(neural_net=None)
+initial_state = game_env.state
+
+mcts_kwargs['GAME_ENV'] = game_env
+MCTS(**mcts_kwargs)
+
+best_child = None
 
 def format_board(board, player=2):
     movable_pieces = []
@@ -48,10 +82,26 @@ def format_board(board, player=2):
 
     return movable_pieces, valid_moves, board_state
 
-@api.post("/get-next-move")
-async def getNextMove(request: Request):
+@api.post("/update-board-with-human-move")
+async def updateHumanMove(request: Request):
+    board = await request.json()
+
+    assert game_env.current_player(game_env.state) == 'player1'
+    move = board['last human move']
+
+    legal_next_states = game_env.legal_next_states
+    moves_list = states_to_piece_positions(legal_next_states)
+    for idx, possible_move in enumerate(moves_list):
+        if move == possible_move:
+            game_env.step(legal_next_states[idx])
+            break
+
+@api.post("/get-ai-move")
+async def getAIMove(request: Request):
     board = await request.json()
     # TODO: call the AI function here
+
+    # assert game_env.current_player(game_env.state) == 'player2'
 
     ai_player = 2
 
@@ -60,6 +110,16 @@ async def getNextMove(request: Request):
     print(np.array(board_state).reshape(-1))
 
     piece_id, tile_id = ai_agent.play(movable_pieces, valid_moves, board_state)
+    # game_env.print_board()
+
+    # if game_env.move_count == 1: # Initialize second player's MCTS node 
+    #     root_node = MCTS_Node(game_env.state, parent=None, initial_state=initial_state)
+    # else: # Update P2 root node with P1's move
+    #     root_node = MCTS.new_root_node(best_child)
+    
+    # MCTS.begin_tree_search(root_node)
+    # best_child = MCTS.best_child(root_node)
+    # game_env.step(best_child.state)
     
     # response should look like this
     return {'pieceId': piece_id, 'tileId': tile_id}
---
+++ utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+# %% Functions
+def get_human_input(game_env):
+    """Print a list of legal next moves for the human player, and return
+    the player's selection.  The player will be presented with move options
+    in the form of two coordinates that represent the start and end location
+    of the piece to be moved.
+    """
+    while True:
+        legal_next_states = game_env.legal_next_states
+        moves_list = states_to_piece_positions(legal_next_states)
+        for idx, move in enumerate(moves_list):
+            print('Option #{}: {} to {}'.format(idx+1, move[0], move[1]))
+        move_idx = int(input('Enter option number: ')) - 1
+        if move_idx in range(len(moves_list)):
+            game_env.step(legal_next_states[move_idx])
+            game_env.print_board()
+            return legal_next_states[move_idx]
+        else:
+            print('Invalid selection!  Try again!')
+
+def states_to_piece_positions(game_env, next_states):
+    """Given a list of next states, produce a list of two coordinates for each 
+    possible next state.  The first coordinate will be the location of the 
+    piece that was moved, and the second coordinate will be the location that 
+    the piece moved to.
+    """
+    moves_list = []
+    state = game_env.state
+    board = state[0] + 2*state[1] + 3*state[2] + 4*state[3]
+    for nstate in next_states:
+        nboard = nstate[0] + 2*nstate[1] + 3*nstate[2] + 4*nstate[3]
+        board_diff = board - nboard
+        xnew, ynew = np.where(board_diff < 0)
+        xnew, ynew = xnew[0], ynew[0]
+        new_val = abs(nboard[xnew,ynew])
+        xold, yold = np.where(board_diff == new_val)
+        try:
+            xold, yold = xold[0], yold[0]
+        except IndexError: # Man promoted to king
+            new_val -= 1 # Value of man is 1 less than king
+            xold, yold = np.where(board_diff == new_val)
+            xold, yold = xold[0], yold[0]
+        moves_list.append([(xold+1,yold+1),(xnew+1,ynew+1)])
+    return moves_list
